5fdd8692e305c1a22770d14840e9df36d6c20a4c
User Name <yoroshikudouzobbb@gmail.com>
2019-12-02 06:44:53 +0900

    テスト修正

--- spec/models/event_chat_spec.rb
+++ spec/models/event_chat_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe EventMsg do
-  it "有効なEventMsgが登録されること" do
-    expect(FactoryBot.create(:event_msg)).to be_valid
+describe EventChat do
+  it "有効なEventChatが登録されること" do
+    expect(FactoryBot.create(:event_chat)).to be_valid
   end
 
   it "メッセージは401文字以上で無効であること" do
-    event_msg = FactoryBot.build(:event_msg,msg: "a"*401)
-    event_msg.valid?
-    expect(event_msg.errors[:msg]).to include "は400文字以内で入力してください"
+    event_chat = FactoryBot.build(:event_chat,chat: "a"*401)
+    event_chat.valid?
+    expect(event_chat.errors[:chat]).to include "は400文字以内で入力してください"
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../config/environment', __dir__)
-
+require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
 require 'capybara/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -63,5 +62,6 @@ RSpec.configure do |config|
 
   #テストヘルパーの使用
   config.include Devise::Test::ControllerHelpers, type: :controller  
-  # config.include RequestSpecHelper, type: :request
+  #config.include RequestSpecHelper, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
